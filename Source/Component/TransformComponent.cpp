ecb678a30e45ec6675518fcaad844c85dcf68ad5
mae0913 <2220158@ecc.ac.jp>
2024-08-14 18:06:59 +0900

    TransformComponent追加処理

--- Source/Component/TransformComponent.cpp
+++ Source/Component/TransformComponent.cpp
@@ -1,6 +1,38 @@
 #include "TransformComponent.h"
 #include <imgui.h>
 
+void TransformComponent::Update(float elapsed_time)
+{
+	// 値が変更されていなければ処理しない
+	if (!this->change_value) return;
+
+	// ワールド行列の更新
+	DirectX::XMVECTOR Q = DirectX::XMLoadFloat4(&rotation);
+	DirectX::XMMATRIX S = DirectX::XMMatrixScaling(scale.x, scale.y, scale.z);
+	DirectX::XMMATRIX R = DirectX::XMMatrixRotationQuaternion(Q);
+	DirectX::XMMATRIX T = DirectX::XMMatrixTranslation(position.x, position.y, position.z);
+
+	DirectX::XMMATRIX W = S * R * T;
+	DirectX::XMStoreFloat4x4(&transform, W);
+}
+
+const DirectX::XMVECTOR TransformComponent::AddPositionVec(const DirectX::XMVECTOR& vec)
+{
+	this->change_value = true;
+	DirectX::XMVECTOR ans = DirectX::XMVectorAdd(vec, DirectX::XMLoadFloat3(&this->position));
+	DirectX::XMStoreFloat3(&this->position, ans);
+	return ans;
+}
+
+const DirectX::XMFLOAT3& TransformComponent::AddPosition(const DirectX::XMFLOAT3 vec)
+{
+	this->change_value = true;
+	this->position.x += vec.x;
+	this->position.y += vec.y;
+	this->position.z += vec.z;
+	return this->position;
+}
+
 #ifdef _DEBUG
 
 void TransformComponent::DrawDebugGUI()
--- Source/Component/TransformComponent.h
+++ Source/Component/TransformComponent.h
@@ -5,16 +5,40 @@
 class TransformComponent : public Component
 {
 public:
-    TransformComponent() :transform() {}
+    TransformComponent() {};
     ~TransformComponent() {};
 
+    void Update(float elapsed_time)override;
     const char* GetName()const override { return "TransformComponent"; }
 
-    const DirectX::XMFLOAT4& GetTransform() { return this->transform; }
-    void SetTransform(const DirectX::XMFLOAT4& transform) { this->transform = transform; }
-private:
-    DirectX::XMFLOAT4 transform;
+    const DirectX::XMFLOAT4X4& GetTransform() { return this->transform; }
+    void SetTransform(const DirectX::XMFLOAT4X4& transform) { this->transform = transform; }
 
+#pragma region set・get parame
+    const DirectX::XMVECTOR GetPositionVec()const noexcept { return DirectX::XMLoadFloat3(&position); }
+    const DirectX::XMFLOAT3& GetPosition()const noexcept { return position; }
+    void SetPosition(const DirectX::XMFLOAT3& pos)noexcept { this->change_value = true; this->position = pos; }
+    void SetPosition(const DirectX::XMVECTOR& pos)noexcept { this->change_value = true; DirectX::XMStoreFloat3(&this->position, pos); }
+    const DirectX::XMVECTOR AddPositionVec(const DirectX::XMVECTOR& vec);
+    const DirectX::XMFLOAT3& AddPosition(const DirectX::XMFLOAT3 vec);
+
+    const DirectX::XMVECTOR GetQuaternionVec()const noexcept { return DirectX::XMLoadFloat4(&rotation); }
+    const DirectX::XMFLOAT4& GetQuaternion()const noexcept { return rotation; }
+    void SetQuaternion(const DirectX::XMVECTOR& quaternion)noexcept { this->change_value = true; DirectX::XMStoreFloat4(&this->rotation, quaternion); }
+    void SetQuaternion(const DirectX::XMFLOAT4& quaternion)noexcept { this->change_value = true; this->rotation = quaternion; }
+
+    const DirectX::XMVECTOR GetScaleVec()const noexcept { return DirectX::XMLoadFloat3(&this->scale); }
+    const DirectX::XMFLOAT3& GetScale()const noexcept { return scale; }
+    void SetScale(const DirectX::XMVECTOR scale)noexcept { this->change_value = true; DirectX::XMStoreFloat3(&this->scale, scale); }
+    void SetScale(const DirectX::XMFLOAT3 scale)noexcept { this->change_value = true; this->scale = scale; }
+
+#pragma endregion set parame
+private:
+    DirectX::XMFLOAT4X4 transform{};
+    DirectX::XMFLOAT3 position{};
+    DirectX::XMFLOAT4 rotation{};
+    DirectX::XMFLOAT3 scale{};
+    bool change_value = false;
 #ifdef _DEBUG
     /**
      * デバックの情報を2D画面に出力する関数
