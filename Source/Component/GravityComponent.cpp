2329d79cc9398b92aef3f6b96676f432c14b160a
mae0913 <2220158@ecc.ac.jp>
2024-10-04 21:13:08 +0900

    GravityComponent追加

---
+++ Source/Component/GravityComponent.cpp
@@ -0,0 +1,26 @@
+#include <imgui.h>
+#include "GravityComponent.h"
+#include "Object/Object.h"
+#include "Component/TransformComponent.h"
+
+void GravityComponent::Update(float elapsed_time)
+{
+    if (this->is_grounded) return;
+
+    auto owner = GetOwner();
+    if (!owner) return;
+    auto transform = owner->EnsureComponentValid<Transform3DComponent>(this->transform_Wptr);
+    if (!transform) return;
+
+    DirectX::XMFLOAT3 pos = transform->GetPosition();
+    pos.y += this->gravity * elapsed_time;
+}
+
+#ifdef _DEBUG
+
+void GravityComponent::DrawDebugGUI()
+{
+    ImGui::Checkbox("Is Grounded", &this->is_grounded);
+}
+
+#endif // _DEBUG
---
+++ Source/Component/GravityComponent.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Component.h"
+
+class Transform3DComponent;
+
+class GravityComponent : public Component
+{
+public:
+    GravityComponent() {};
+
+    // 更新関数
+    void Update(float elapsed_time);
+
+    // 名前取得
+    const char* GetName()const { return "GravityComponent"; };
+
+    // 各種取得・設定関数
+    const bool& GetIsGrounded() { return this->is_grounded; }
+    void SetIsGrounded(bool is_grounded) { this->is_grounded = is_grounded; }
+private:
+    float gravity = -9.8f;   // 重力加速度
+    bool is_grounded = false;
+private:
+    std::weak_ptr<Transform3DComponent> transform_Wptr;
+
+#ifdef _DEBUG
+public:
+    void DrawDebugGUI();
+
+#endif // _DEBUG
+};
