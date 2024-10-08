e387dfee5c1e48f699c8abfcc39fd3b4973e7802
mae0913 <2220158@ecc.ac.jp>
2024-10-08 21:16:29 +0900

    MYVECTOR3仮完成

--- Source/System/MYVECTOR/MYVECTOR3.h
+++ Source/System/MYVECTOR/MYVECTOR3.h
@@ -64,9 +64,14 @@ public:
         f3.z = z;
         this->vector = DirectX::XMLoadFloat3(&f3);
     }
+
     void ZeroClear()
     {
         vector = DirectX::XMVectorZero();
+    }    
+    void ClearToValu(float valu)
+    {
+        vector = DirectX::XMVectorSet(valu, valu, valu, 0.0f);
     }
 
     // 長さを計算
@@ -74,10 +79,97 @@ public:
     {
         return DirectX::XMVectorGetX(DirectX::XMVector3Length(this->vector));
     }
+    float LengthSp() const
+    {
+        return DirectX::XMVectorGetX(DirectX::XMVector3LengthSq(this->vector));
+    }
     // 正規化
     MYVECTOR3 Normalize() const
     {
         return MYVECTOR3(DirectX::XMVector3Normalize(this->vector));
+    }
+    // 各値の絶対値を計算
+    MYVECTOR3 Abs() const
+    {
+        return MYVECTOR3(DirectX::XMVectorAbs(this->vector));
+    }
+    // 数値を切り上げて整数にする
+    MYVECTOR3 Ceiling() const
+    {
+        return MYVECTOR3(DirectX::XMVectorCeiling(this->vector));
+    }
+    // 数値を切り捨てて整数にする
+    MYVECTOR3 Floor() const
+    {
+        return MYVECTOR3(DirectX::XMVectorFloor(this->vector));
+    }
+    // 符号を反転させる
+    MYVECTOR3 Negate() const
+    {
+        return MYVECTOR3(DirectX::XMVectorNegate(this->vector));
+    }
+    // 最も近い整数に丸める
+    MYVECTOR3 Round() const
+    {
+        return MYVECTOR3(DirectX::XMVectorRound(this->vector));
+    }
+    // 0.0f ～ 1.0fに納める
+    MYVECTOR3 Saturate() const
+    {
+        return MYVECTOR3(DirectX::XMVectorSaturate(this->vector));
+    }
+    // 平方根を計算
+    MYVECTOR3 Sqrt() const
+    {
+        return MYVECTOR3(DirectX::XMVectorSqrt(this->vector));
+    }
+    float Sum() const
+    {
+        return DirectX::XMVectorGetX(DirectX::XMVectorSum(DirectX::XMVectorMultiply(this->vector, DirectX::XMVectorSet(1.0f, 1.0f, 1.0f, 0.0))));
+    }
+
+    // クランプ
+    MYVECTOR3 Clamp(MYVECTOR3 min, MYVECTOR3 max) const
+    {
+        return DirectX::XMVectorClamp(this->vector, min.GetVector(), max.GetVector());
+    }
+    MYVECTOR3 Min(MYVECTOR3 min) const
+    {
+        return DirectX::XMVectorMin(this->vector, min.GetVector());
+    }
+    MYVECTOR3 Max(MYVECTOR3 max) const
+    {
+        return DirectX::XMVectorMax(this->vector, max.GetVector());
+    }
+    // 整数の除算の余りを求める
+    MYVECTOR3 Mod(MYVECTOR3 mVec) const
+    {
+        return DirectX::XMVectorMod(this->vector, mVec.GetVector());
+    }
+    // 累乗
+    MYVECTOR3 Pow(MYVECTOR3 mVec) const
+    {
+        return MYVECTOR3(DirectX::XMVectorPow(this->vector, mVec.GetVector()));
+    }
+    // 累乗
+    MYVECTOR3 Pow(float f) const
+    {
+        return MYVECTOR3(DirectX::XMVectorPow(this->vector, DirectX::XMVectorSet(f, f, f, 0.0f)));
+    }
+    // 内積計算
+    MYVECTOR3 Dot(MYVECTOR3 mVec) const
+    {
+        return MYVECTOR3(DirectX::XMVector3Dot(this->vector, mVec.GetVector()));
+    }
+    // 外積計算
+    MYVECTOR3 Cross(MYVECTOR3 mVec) const
+    {
+        return MYVECTOR3(DirectX::XMVector3Cross(this->vector, mVec.GetVector()));
+    }
+    // 線形補間
+    MYVECTOR3 Lerp(MYVECTOR3 mVec,float t) const
+    {
+        return MYVECTOR3(DirectX::XMVectorLerp(this->vector, mVec.GetVector(), t));
     }
 
     // 加算演算子のオーバーロード
@@ -90,18 +182,25 @@ public:
     {
         return MYVECTOR3(DirectX::XMVectorSubtract(this->vector, other.GetVector()));
     }
-    // 乗算
-    MYVECTOR3 const operator/(MYVECTOR3 other)
+    // 乗算演算子のオーバーロード
+    MYVECTOR3 const operator*(MYVECTOR3 other)
     {
         return MYVECTOR3(DirectX::XMVectorMultiply(this->vector, other.GetVector()));
     }
-
     // スカラー倍
     MYVECTOR3 const operator*(float scalar)
     {
         return MYVECTOR3(DirectX::XMVectorScale(this->vector, scalar));
     }
+    // 除算演算子のオーバーロード
+    MYVECTOR3 const operator/(MYVECTOR3 other)
+    {
+        return MYVECTOR3(DirectX::XMVectorDivide(this->vector, other.GetVector()));
+    }
 
+#pragma region VectorXZ
+    // TODO 作成する
+#pragma endregion VectorXZ
 private:
     alignas(16) DirectX::XMVECTOR vector;  // 内部でXMVECTORを保持
 };
@@ -115,3 +214,5 @@ class T
 
      MYVECTOR3 vector;
 };
+
+// XZ2軸作成
