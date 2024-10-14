613abc89a4819b3d6ddd10b76866dff28d1ff06e
mae0913 <2220158@ecc.ac.jp>
2024-10-14 10:20:49 +0900

    StateTransitionInfoをクラス化

--- Source/StateMachine/State.cpp
+++ Source/StateMachine/State.cpp
@@ -1,6 +1,16 @@
 #include "State.h"
 #include "Model/AnimeTransitionJudgement.h"
 
+StateTransitionInfo::StateTransitionInfo(
+    std::string next_state_name,
+    std::unique_ptr<AnimeTransitionJudgementBase> judgement
+)
+    :next_state_index(-1),
+    next_state_name(next_state_name),
+    judgement(std::move(judgement))
+{
+}
+
 void StateBase::PreTransitionJudgemen()
 {
     for (auto& state : this->pre_update_judgement_pool)
@@ -25,16 +35,15 @@ bool StateBase::PerformTransitionJudgement(AnimeTransitionJudgementBase* judgeme
 #endif // _DEBUG
 
     // 遷移準備を待つフラグがオンの場合、遷移の準備が整うまで待機する
-    if (judgemen->GetRequireTransitionReady()) return false;
+    if (judgemen->GetRequireTransitionReady() && !IsTransitionReady()) return false;
 
     return judgemen->GetShouldReversey() ? !judgemen->CheckTransitionCondition() : judgemen->CheckTransitionCondition();
 }
 
-void StateBase::AddStateTransition(StateIndex state_index, std::unique_ptr<AnimeTransitionJudgementBase> judgement, JudgementUpdatePhase phase)
+void StateBase::AddStateTransition(std::unique_ptr<StateTransitionInfo> state_transition_info, JudgementUpdatePhase phase)
 {
     auto& judgement_pool = (phase == JudgementUpdatePhase::PreUpdate) ?
         pre_update_judgement_pool : post_update_judgement_pool;
     auto& state_transition = judgement_pool.emplace_back();
-    state_transition.state_index = state_index;
-    state_transition.judgement = std::move(judgement);
+    state_transition = std::move(state_transition_info);
 }
--- Source/StateMachine/State.h
+++ Source/StateMachine/State.h
@@ -1,20 +1,31 @@
 #pragma once
 #include <vector>
 #include <memory>
+#include <string>
 
 class AnimeTransitionJudgementBase;
 using StateIndex = size_t;
 
+// ステートの遷移情報
+class StateTransitionInfo
+{
+public:
+    StateTransitionInfo(std::string next_state_name, std::unique_ptr<AnimeTransitionJudgementBase> judgement);
+    ~StateTransitionInfo() {};
+
+    StateIndex GetNextStateIndex() { return this->next_state_index; }
+    std::string GetNextStateName() { return this->next_state_name; }
+    const char* GetNextStateNameC() { return this->next_state_name.c_str(); }
+    AnimeTransitionJudgementBase* GetJudgement() { return judgement.get(); }
+private:
+    StateIndex next_state_index;       // 遷移先のステートのインデックス
+    std::string next_state_name;          // 遷移先のステートの名前
+    std::unique_ptr<AnimeTransitionJudgementBase> judgement;	// 遷移判定
+};
+
 class StateBase
 {
 public:
-    // ステートの遷移情報
-    struct StateTransitionInfo
-    {
-        StateIndex state_index;
-        std::unique_ptr<AnimeTransitionJudgementBase> judgement;	// 遷移判定
-    };
-
     enum class JudgementUpdatePhase
     {
         PreUpdate,      // 更新前の状態
@@ -32,6 +43,10 @@ public:
 
     virtual const char* Name() = 0;
 
+    // 遷移準備が完了しているか
+    // 遷移判定クラスで遷移準備を待つ設定の時に使用する
+    virtual bool IsTransitionReady() { return true; };
+
     // 更新関数の前の遷移判定
     void PreTransitionJudgemen();
     // 更新関数の後の遷移判定
@@ -42,10 +57,10 @@ public:
     bool PerformTransitionJudgement(AnimeTransitionJudgementBase* judgemen);
     
     // 遷移ステートの追加
-    void AddStateTransition(StateIndex state_index, std::unique_ptr<AnimeTransitionJudgementBase> judgement, JudgementUpdatePhase phase);
+    void AddStateTransition(std::unique_ptr<StateTransitionInfo> state_transition, JudgementUpdatePhase phase);
 private:
-    std::vector<StateTransitionInfo> pre_update_judgement_pool;     // Update前に遷移判定を行う
-    std::vector<StateTransitionInfo> post_update_judgement_pool;    // Update後に遷移判定を行う
+    std::vector<std::unique_ptr<StateTransitionInfo>> pre_update_judgement_pool;     // Update前に遷移判定を行う
+    std::vector<std::unique_ptr<StateTransitionInfo>> post_update_judgement_pool;    // Update後に遷移判定を行う
 #ifdef _DEBUG
 public:
     virtual void DrawDebugGUI() {};
