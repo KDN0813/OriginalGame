1f054d3d0f2bf648f3d55e32acb8cd9437960b15
mae0913 <2220158@ecc.ac.jp>
2024-10-16 08:21:55 +0900

    CircleCollider作成

--- Source/Collision/CircleCollider.cpp
+++ Source/Collision/CircleCollider.cpp
@@ -1,1 +1,17 @@
 #include "CircleCollider.h"
+
+void CircleCollider::Update()
+{
+}
+
+void CircleCollider::AddCircle(std::shared_ptr<CircleComponent> circle)
+{
+}
+
+void CircleCollider::CheckCollision()
+{
+}
+
+void CircleCollider::RemoveDeletedCircle()
+{
+}
--- Source/Collision/CircleCollider.h
+++ Source/Collision/CircleCollider.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <memory>
+#include <vector>
+
+class CircleComponent;
 
 // 円の当たり判定
 class CircleCollider
@@ -7,6 +11,18 @@ public:
     CircleCollider() {};
     ~CircleCollider() {};
 
+    // 外部で呼ぶ更新関数
+    void Update();
+
+    // 当たり判定を行うcircleを追加する関数
+    void AddCircle(std::shared_ptr<CircleComponent> circle);
 private:
+    // 当たり判定を行う関数
+    void CheckCollision();
+
+    // 既に削除されたcircleのコンテナから取り除く関数
+    void RemoveDeletedCircle();
+private:
+    std::vector<std::weak_ptr<CircleComponent>> circle_pool;
 };
 
