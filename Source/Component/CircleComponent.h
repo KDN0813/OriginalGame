a8f7383e0b525a4abbb2d92488f5668cfe55df64
mae0913 <2220158@ecc.ac.jp>
2024-10-16 10:24:59 +0900

    CircleComponentにCOLLISION_TYPEとTARGET_TYPEの列挙型追加

--- Source/Component/CircleComponent.h
+++ Source/Component/CircleComponent.h
@@ -1,6 +1,29 @@
 #pragma once
 #include "Component.h"
 
+// コライダーの種類を定義する列挙型
+// オブジェクトが衝突の際にどの役割を果たすかを指定する
+enum class COLLISION_TYPE
+{
+    NONE = 0,           // 当たり判定なし
+    ATTACKER = 1 << 0,  // ぶつける側（アクティブに衝突を引き起こすオブジェクト）
+    DEFENDER = 1 << 1,  // ぶつけられる側（衝突されるオブジェクト）
+    BOTH = 1 << 2,      // 両方に当たり判定がある場合（ぶつける側とぶつけられる側の両方）
+    DEFAULT = BOTH,     // デフォルトでは両方に判定を行う
+};
+
+// 対象となるターゲットの種類を定義する列挙型
+// ATTACKER（攻撃者）および DEFENDER（防御者）で使用し、
+// それぞれどのタイプの相手をターゲットとするかを指定する
+enum class TARGET_TYPE
+{
+    NONE = 0,           // ターゲットなし
+    PLAYER = 1 << 0,    // プレイヤーをターゲットとする
+    ENEMY = 1 << 1,     // 敵をターゲットとする
+    ALL = 1 << 2,       // すべてのターゲット
+    DEFAULT = ALL       // デフォルトではすべてのターゲットを指定
+};
+
 // 円の当たり判定用コンポーネント
 class CircleComponent : public Component
 {
