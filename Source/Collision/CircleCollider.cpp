13d9944af23759d12a0f5e6382cf7efc0b04c6c1
mae0913 <2220158@ecc.ac.jp>
2024-10-17 10:00:57 +0900

    CheckCollision関数をCircleCollisionに対応させる

--- Source/Collision/CircleCollider.cpp
+++ Source/Collision/CircleCollider.cpp
@@ -34,50 +34,82 @@ void CircleCollider::AddCircle(std::shared_ptr<CircleComponent>& circle)
 void CircleCollider::CheckCollision()
 {
     // 実行可能なattackerの取得
-    std::vector<std::shared_ptr<CircleComponent>> active_attacker_pool;
-    for (std::weak_ptr<CircleComponent> attacker_Wptr : this->circle_attacker_pool)
+    std::vector<std::vector<std::shared_ptr<CircleComponent>>> active_attacker_pool_pool;
+    std::vector<bool> is_active_defender_object; // あてられる側のオブジェクトが必用であるか
+
+    const size_t CircleCollisionMax = this->circle_collision_pool.size();
+    active_attacker_pool_pool.resize(CircleCollisionMax);
+    is_active_defender_object.resize(CircleCollisionMax);
+
+    // 攻撃側の中から判定を行うものをコンテナに集める
+    for (size_t i = 0; i < CircleCollisionMax; ++i)
     {
-        std::shared_ptr<CircleComponent> attacker = attacker_Wptr.lock();
-        if (!attacker)
+        for (std::weak_ptr<CircleComponent> attacker_Wptr : this->circle_collision_pool[i].circle_attacker_pool)
         {
-            // 削除コンテナに追加
-            circle_remove_pool.emplace_back(attacker_Wptr);
-            continue;
-        }
-        if (!attacker->GetIsActive()) continue;
-
-        active_attacker_pool.emplace_back(attacker);
-    }
-
-    // 実行可能なdeffenderの取得
-    std::vector<std::shared_ptr<CircleComponent>> active_deffender_pool;
-    for (std::weak_ptr<CircleComponent> deffender_Wptr : this->circle_deffender_pool)
-    {
-        std::shared_ptr<CircleComponent> deffender = deffender_Wptr.lock();
-        if (!deffender)
-        {
-            // 削除コンテナに追加
-            circle_remove_pool.emplace_back(deffender_Wptr);
-            continue;
-        }
-        if (!deffender->GetIsActive()) continue;
-
-        active_deffender_pool.emplace_back(deffender);
-    }
-
-    for (std::shared_ptr<CircleComponent>& attacker : active_attacker_pool)
-    {
-        for (std::shared_ptr<CircleComponent>& deffender : active_deffender_pool)
-        {
-            // 判定を行う
-            CircleHitResult hit;
-            if (Collision::IntersectCircleVsCircle(attacker->GetCircleParam(), deffender->GetCircleParam(), hit))
+            // 有効であるか判定
+            std::shared_ptr<CircleComponent> attacker = attacker_Wptr.lock();
+            if (!attacker)
             {
-                attacker->SetHitFlag(true);
-                deffender->SetHitFlag(true);
+                // 削除コンテナに追加
+                continue;
+            }
+            if (!attacker->GetIsActive()) continue;
 
-                hit.hit_object_Wptr = deffender->GetOwner();
-                attacker->SetHitResult(hit);
+            active_attacker_pool_pool[i].emplace_back(attacker);
+
+            // ターゲットのオブジェクトタイプの受け側の使用flagを立てる
+            is_active_defender_object[static_cast<size_t>(attacker->GetTargetType())] = true;
+        }
+    }
+    
+    // 有効な受け側のデータ取得
+    std::vector<std::vector<std::shared_ptr<CircleComponent>>> active_deffender_pool_pool;
+
+    active_deffender_pool_pool.resize(CircleCollisionMax);
+
+    for (size_t i = 0; i < is_active_defender_object.size(); ++i)
+    {
+        // 現在のタイプの必用であるフラグが立っていない場合飛ばす
+        if (!is_active_defender_object[i]) continue;
+
+        for (std::weak_ptr<CircleComponent> deffender_Wptr : this->circle_collision_pool[i].circle_defender_pool)
+        {
+            std::shared_ptr<CircleComponent> deffender = deffender_Wptr.lock();
+            if (!deffender)
+            {
+                // 削除コンテナに追加
+                continue;
+            }
+            if (!deffender->GetIsActive()) continue;
+
+            active_deffender_pool_pool.emplace_back(deffender);
+        }
+    }
+
+    // 各種類の当てる側のコンテナを判定
+    for (std::vector<std::shared_ptr<CircleComponent>> active_attacker_pool : active_attacker_pool_pool)
+    {
+        // 当てる側のCircleComponentを取得
+        for (std::shared_ptr<CircleComponent> attacker : active_attacker_pool)
+        {
+
+            // 各種類の受ける側のコンテナを判定
+            for (std::vector<std::shared_ptr<CircleComponent>> active_deffender_pool : active_deffender_pool_pool)
+            {
+                // 受ける側のCircleComponentを取得
+                for (std::shared_ptr<CircleComponent>& deffender : active_deffender_pool)
+                {
+                    // 判定を行う
+                    CircleHitResult hit;
+                    if (Collision::IntersectCircleVsCircle(attacker->GetCircleParam(), deffender->GetCircleParam(), hit))
+                    {
+                        attacker->SetHitFlag(true);
+                        deffender->SetHitFlag(true);
+
+                        hit.hit_object_Wptr = deffender->GetOwner();
+                        attacker->SetHitResult(hit);
+                    }
+                }
             }
         }
     }
