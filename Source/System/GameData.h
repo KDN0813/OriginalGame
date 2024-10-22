69fb4bf246cd61c4410af63b3ad9e9be4e737765
mae0913 <2220158@ecc.ac.jp>
2024-10-23 08:41:06 +0900

    GameDataに必要なパラメータを持たせる

--- Source/System/GameData.h
+++ Source/System/GameData.h
@@ -3,5 +3,18 @@
 
 class GameData : public Singleton<GameData>
 {
+public:
+    GameData() :Singleton(this) {};
+    ~GameData() {};
 
+    void ClearParam() { this->score = 0; this->game_elapsed_time = 0.0f; }
+
+    // 各種取得・設定関数
+    int GetScore()const { return this->score; }
+    float GetGameElapsedTime()const { return this->game_elapsed_time; }
+    void SetScore(int s) { this->score = s; }
+    void SetGameElapsedTime(float t) { this->game_elapsed_time = t; }
+private:
+    int score = 0;              // スコア
+    float game_elapsed_time = 0.0f;  // 経過時間
 };
