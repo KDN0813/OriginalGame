d7c576731df186492c5012a730bbe49682351bef
mae0913 <2220158@ecc.ac.jp>
2024-10-23 15:43:54 +0900

    AudioResourceを追加

--- Source/Audio/Audio.cpp
+++ Source/Audio/Audio.cpp
@@ -1,4 +1,5 @@
 #include "Audio.h"
+#include "System/Misc.h"
 #ifdef _DEBUG
 #include <imgui.h>
 #endif // DEBUG
@@ -7,6 +8,24 @@
 Audio::Audio()
     :Singleton(this)
 {
+	HRESULT hr;
+
+	// COMの初期化
+	hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	_ASSERT_EXPR(SUCCEEDED(hr), HRTrace(hr));
+
+	UINT32 create_flags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+	//createFlags |= XAUDIO2_DEBUG_ENGINE;
+#endif
+
+	// XAudio初期化
+	hr = XAudio2Create(&this->xaudio, create_flags);
+	_ASSERT_EXPR(SUCCEEDED(hr), HRTrace(hr));
+
+	// マスタリングボイス生成
+	hr = xaudio->CreateMasteringVoice(&this->masteringVoice);
+	_ASSERT_EXPR(SUCCEEDED(hr), HRTrace(hr));
 }
 
 #ifdef _DEBUG
--- Source/Audio/Audio.h
+++ Source/Audio/Audio.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <xaudio2.h>
 #include "System/ClassBase/Singleton.h"
 
 class Audio : public Singleton<Audio>
@@ -8,6 +9,8 @@ public:
     ~Audio() {};
 
 private:
+    IXAudio2* xaudio = nullptr;
+    IXAudio2MasteringVoice* masteringVoice = nullptr;
 
 #ifdef _DEBUG
 public:
