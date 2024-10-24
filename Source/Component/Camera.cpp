891211e5d1497c9473e7a201138ff28c48779c67
mae0913 <2220158@ecc.ac.jp>
2024-10-24 19:51:41 +0900

    Cameraクラス中身作成

--- Source/Component/Camera.cpp
+++ Source/Component/Camera.cpp
@@ -1,1 +1,107 @@
+#include <imgui.h>
+#include "System/MyMath/MYMATRIX.h"
 #include "Camera.h"
+#include "Object/Object.h"
+#include "Camera/CameraManager.h"
+
+#include "Component/TransformComponent.h"
+
+Camera::Camera(const CameraParam& camera_param)
+    : camera_param(camera_param), default_param(camera_param)
+{
+}
+
+void Camera::Start()
+{
+}
+
+void Camera::End()
+{
+}
+
+void Camera::ReStart()
+{
+    this->camera_param = this->default_param;
+    change_value = true;
+}
+
+void Camera::Update(float elapsed_time)
+{
+    if (!this->change_value) return;
+
+    float sx = ::sinf(this->camera_param.rotateX), cx = ::cosf(this->camera_param.rotateX);
+    float sy = ::sinf(this->camera_param.rotateY), cy = ::cosf(this->camera_param.rotateY);
+    MYVECTOR3 Front = MYVECTOR3(-cx * sy, -sx, -cx * cy);
+    MYVECTOR3 Focus = this->camera_param.focus;
+    MYVECTOR3 Eye = Focus - Front * this->camera_param.range;
+    MYVECTOR3 Up = MYVECTOR3(0.0f, 1.0f, 0.0f);
+
+    SetLookAt(Eye, Focus, Up);
+    SetPerspectiveFov(this->camera_param.fovY, this->camera_param.aspect, this->camera_param.nearZ, this->camera_param.farZ);
+}
+
+void Camera::SetMainCamera()
+{
+}
+
+void Camera::SetLookAt(MYVECTOR3 Eye, MYVECTOR3 Focus, MYVECTOR3 Up)
+{
+    // 視点、中視点、上方向からビューを行列を作成
+    MYMATRIX View_transform;
+    View_transform.SetLookAtLH(Eye, Focus, Up);
+    View_transform.GetFlaot4x4(this->view_transform);
+
+    // ビューを逆行列化し、ワールド行列に戻す
+    MYMATRIX World_transform = View_transform.GetInverse(nullptr);
+
+    // カメラ方向を取り出す
+    World_transform.GetRight().GetFlaot3(this->camera_param.right);
+    World_transform.GetUp().GetFlaot3(this->camera_param.up);
+    World_transform.GetForward().GetFlaot3(this->camera_param.forward);
+
+    // 視点、注視点を保存
+    Eye.GetFlaot3(this->camera_param.eye);
+    Focus.GetFlaot3(this->camera_param.focus);
+}
+
+void Camera::SetPerspectiveFov(float fovY, float aspect, float nearX, float farZ)
+{
+    // 画面比率、クリップ距離からプロジェクション行列を作成
+    MYMATRIX Projection_transform;
+    Projection_transform.SetPerspectiveFovLH(fovY, aspect, nearX, farZ);
+    Projection_transform.GetFlaot4x4(this->projection_transform);
+}
+
+#ifdef _DEBUG
+
+void Camera::DrawDebugGUI()
+{
+    if (ImGui::SliderFloat("front_range", &this->camera_param.range, 1.0f, 50.0f))
+    {
+        this->change_value = true;
+    }
+    if (ImGui::SliderFloat("rotateY", &this->camera_param.rotateY, -DirectX::XM_PI, DirectX::XM_PI))
+    {
+        this->change_value = true;
+    }
+    constexpr float RotateX_Max = DirectX::XMConvertToRadians(89.0f);
+    if (ImGui::SliderFloat("rotateX", &this->camera_param.rotateX, -RotateX_Max, RotateX_Max))
+    {
+        this->change_value = true;
+    }
+
+    if (ImGui::InputFloat3("focus", &this->camera_param.focus.x))
+    {
+        this->change_value = true;
+    }
+
+    ImGui::Checkbox("Is Main Camera", &this->is_main_camera);
+
+
+    if (ImGui::Button("SetMainCamera"))
+    {
+        SetMainCamera();
+    }
+}
+
+#endif // _DEBUG
--- Source/Component/Camera.h
+++ Source/Component/Camera.h
@@ -1,7 +1,131 @@
 #pragma once
+#include <memory>
+#include "System/MyMath/MYVECTOR3.h"
 #include "Component.h"
-class Camera :
-    public Component
+
+class CameraManager;
+class Transform3DComponent;
+
+class Camera : public Component
 {
+public:
+	struct CameraParam
+	{
+		DirectX::XMFLOAT3		eye = DirectX::XMFLOAT3(0.0f, 0.0f, -1.0f);
+		DirectX::XMFLOAT3		focus = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
+		DirectX::XMFLOAT3		right = DirectX::XMFLOAT3(1.0f, 0.0f, 0.0f);
+		DirectX::XMFLOAT3		up = DirectX::XMFLOAT3(0.0f, 1.0f, 0.0f);
+		DirectX::XMFLOAT3		forward = DirectX::XMFLOAT3(0.0f, 0.0f, 1.0f);
+		float					fovY = DirectX::XMConvertToRadians(45);
+		float					aspect = 16.0f / 9.0f;
+		float					nearZ = 0.1f;
+		float					farZ = 1000.0f;
+		float					range = 1.0f;
+		float					rotateY = 0.0f;
+		float					rotateX = 0.0f;
+	};
+public:
+	Camera(const CameraParam& camera_param);
+
+	// 開始関数
+	void Start()override;
+	// 修了関数
+	void End()override;
+	// リスタート処理
+	void ReStart();
+	// 更新関数
+	void Update(float elapsed_time);
+	// 名前取得
+	const char* GetName()const { return "CameraComponent"; };
+	// 優先度
+	const COMPONENT_PRIORITY GetPriority()const noexcept override { return COMPONENT_PRIORITY::LOWEST; }
+
+	/**
+	 * \fn SetMainCamera
+	 * \brief メインカメラに設定する
+	 *
+	 */
+	void SetMainCamera();
+
+	// 視点設定
+	void SetEye(DirectX::XMFLOAT3 eye) { this->change_value = true; this->camera_param.eye = eye; }
+	// 注視店設定
+	void SetFocus(DirectX::XMFLOAT3 focus) { this->change_value = true; this->camera_param.focus = focus; }
+	// 右方向設定
+	void  SetRight(DirectX::XMFLOAT3 right) { this->change_value = true; this->camera_param.right = right; }
+	// 上方向設定
+	void SetUp(DirectX::XMFLOAT3 up) { this->change_value = true; this->camera_param.up = up; }
+	// 前方向設定
+	void SetForward(DirectX::XMFLOAT3 forward) { this->change_value = true; this->camera_param.forward = forward; }
+	// 画角設定
+	void SetFovY(float fovY) { this->change_value = true; this->camera_param.fovY = fovY; }
+	// アスペクト比設定
+	void SetAspect(float aspect) { this->change_value = true; this->camera_param.aspect = aspect; }
+	// ニアクリップ値設定
+	void SetNearZ(float nearZ) { this->change_value = true; this->camera_param.nearZ = nearZ; }
+	// ファークリップ値設定
+	void SetFarZ(float farZ) { this->change_value = true; this->camera_param.farZ = farZ; }
+	// カメラの距離設定
+	void SetRange(float range) { this->change_value = true; this->camera_param.range = range; }
+	// Y軸回転度設定
+	void SetRotateY(float rotateY) { this->change_value = true; this->camera_param.rotateY = rotateY; }
+	// X軸回転度設定
+	void SetRotateX(float rotateX) { this->change_value = true; this->camera_param.rotateX = rotateX; }
+	// メインカメラフラグの設定
+	void SetIsMainCamera(bool is_main_camera) { this->is_main_camera = is_main_camera; }
+
+	// ビュー行列取得
+	DirectX::XMFLOAT4X4 GetViewTransform() const { return this->view_transform; }
+	// プロジェクション行列取得
+	DirectX::XMFLOAT4X4 GetProjectionTransform() const { return this->projection_transform; }
+	// 視点取得
+	DirectX::XMFLOAT3 GetEye() const { return this->camera_param.eye; }
+	// 注視店取得
+	DirectX::XMFLOAT3 GetFocus() const { return this->camera_param.focus; }
+	// 右方向取得
+	DirectX::XMFLOAT3 GetRight() const { return this->camera_param.right; }
+	// 上方向取得
+	DirectX::XMFLOAT3 GetUp() const { return this->camera_param.up; }
+	// 前方向取得
+	DirectX::XMFLOAT3 GetForward() const { return this->camera_param.forward; }
+	// 画角取得
+	float GetFovY() { return this->camera_param.fovY; }
+	// アスペクト比取得
+	float GetAspect() { return this->camera_param.aspect; }
+	// ニアクリップ値取得
+	float GetNearZ() { return this->camera_param.nearZ; }
+	// ファークリップ値取得
+	float GetFarZ() { return this->camera_param.farZ; }
+	// カメラの距離取得
+	float GetRange() { return this->camera_param.range; }
+	// Y軸回転度取得
+	float GetRotateY() { return this->camera_param.rotateY; }
+	// X軸回転度取得
+	float GetRotateX() { return this->camera_param.rotateX; }
+	// メインカメラであるか
+	bool GetIsMainCamera() { return this->is_main_camera; }
+private:
+	// 指定方向を向く
+	void SetLookAt(MYVECTOR3 Eye, MYVECTOR3 Focus, MYVECTOR3 Up);
+	// パースペクティブ設定
+	void SetPerspectiveFov(float fovY, float aspect, float nearX, float farZ);
+
+private:
+	CameraParam camera_param;
+	CameraParam default_param;
+	DirectX::XMFLOAT4X4	view_transform = {};
+	DirectX::XMFLOAT4X4	projection_transform = {};
+
+	bool change_value = true;
+	bool is_main_camera = false;
+	CameraManager* camera_manager;
+private:
+	std::weak_ptr<Transform3DComponent> transform_Wptr;
+
+#ifdef _DEBUG
+public:
+	void DrawDebugGUI();
+
+#endif // _DEBUG
 };
 
