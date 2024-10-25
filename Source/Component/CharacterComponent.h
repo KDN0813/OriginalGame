0219a5f3e4f61f0745672564b9925e3e97b8f790
mae0913 <2220158@ecc.ac.jp>
2024-10-25 11:30:33 +0900

    CharacterComponent作成

---
+++ Source/Component/CharacterComponent.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Component.h"
+
+class CharacterComponent : public Component
+{
+public:
+    CharacterComponent() {};
+    ~CharacterComponent() {};
+
+    // 開始関数
+    void Start()  override {};
+    // 終了関数
+    void End()  override {};
+    // リスタート処理
+    void ReStart() override {};      // パラメータの初期化
+    // 更新関数
+    void Update(float elapsed_time) override {};
+
+    // 名前取得
+    const char* GetName()const  override { return "CharacterComponent"; };
+
+    // 優先度
+    const COMPONENT_PRIORITY GetPriority()const noexcept  override { return COMPONENT_PRIORITY::DEFAULT; };
+
+#ifdef _DEBUG
+public:
+    /**
+     * デバックの情報を2D画面に出力する関数
+     */
+    void DrawDebugGUI()  override {};
+    /**
+     * デバックの情報を3D画面上に出力する関数
+     */
+    void DrawDebugPrimitive()  override {};
+    /**
+     * デバッグプリミティブ表示用ImGui
+     */
+    void DrawDebugPrimitiveGUI()  override {};
+    bool IsDebugPrimitive() override { return false; }   // DebugPrimitiveが存在するか
+#endif // DEBUG
+};
+
