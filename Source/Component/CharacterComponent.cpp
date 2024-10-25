dd7a46a265b98ce86bb0d842f5ea4967f7d0027f
mae0913 <2220158@ecc.ac.jp>
2024-10-25 11:44:48 +0900

    CharacterComponent作成

--- Source/Component/CharacterComponent.cpp
+++ Source/Component/CharacterComponent.cpp
@@ -1,1 +1,45 @@
 #include "CharacterComponent.h"
+#ifdef _DEBUG
+#include <imgui.h>
+#endif // _DEBUG
+
+
+CharacterComponent::CharacterComponent(CharacterParam param)
+    :param(param), default_param(param)
+{
+}
+
+void CharacterComponent::ReStart()
+{
+    this->param = this->default_param;
+}
+
+void CharacterComponent::ApplyDamage(int damege)
+{
+    this->param.hp = (std::max)(this->param.hp - damege, 0);
+}
+
+bool CharacterComponent::IsAlive()
+{
+    return (0 < this->param.hp);    // 体力が0以上なら生存
+}
+
+#ifdef _DEBUG
+
+void CharacterComponent::DrawDebugGUI()
+{
+    if (ImGui::InputInt("HP", &this->param.hp)) this->param.hp = (std::min)((std::max)(this->param.hp, 0), this->param.max_hp);
+    ImGui::InputInt("Max HP", &this->param.max_hp);
+
+    // ダメージテスト
+    ImGui::InputInt("Test Damage", &this->test_damage);
+    if (ImGui::Button("Test ApplyDamage"))
+    {
+        ApplyDamage(this->test_damage);
+    }
+
+    bool is_alive = IsAlive();
+    ImGui::Checkbox("Is Alive", &is_alive);
+}
+
+#endif // _DEBUG
--- Source/Component/CharacterComponent.h
+++ Source/Component/CharacterComponent.h
@@ -4,7 +4,13 @@
 class CharacterComponent : public Component
 {
 public:
-    CharacterComponent() {};
+    struct CharacterParam
+    {
+        int hp = 1;     // 体力
+        int max_hp = 1; // 最大体力
+    };
+public:
+    CharacterComponent(CharacterParam param);
     ~CharacterComponent() {};
 
     // 開始関数
@@ -12,22 +18,30 @@ public:
     // 終了関数
     void End()  override {};
     // リスタート処理
-    void ReStart() override {};      // パラメータの初期化
+    void ReStart() override;      // パラメータの初期化
     // 更新関数
     void Update(float elapsed_time) override {};
 
     // 名前取得
     const char* GetName()const  override { return "CharacterComponent"; };
 
+    // ダメージを適用する
+    void ApplyDamage(int damege);
+    // 生存しているか
+    bool IsAlive();
+
     // 優先度
     const COMPONENT_PRIORITY GetPriority()const noexcept  override { return COMPONENT_PRIORITY::DEFAULT; };
+private:
+    CharacterParam param;
+    CharacterParam default_param;
 
 #ifdef _DEBUG
 public:
     /**
      * デバックの情報を2D画面に出力する関数
      */
-    void DrawDebugGUI()  override {};
+    void DrawDebugGUI()  override;
     /**
      * デバックの情報を3D画面上に出力する関数
      */
@@ -37,6 +51,9 @@ public:
      */
     void DrawDebugPrimitiveGUI()  override {};
     bool IsDebugPrimitive() override { return false; }   // DebugPrimitiveが存在するか
+
+private:
+    int test_damage = 1;    // テストで与えるダメージ量
 #endif // DEBUG
 };
 
