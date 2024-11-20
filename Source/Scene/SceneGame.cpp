14048375328f115d5208243a54b8015e72641233
mae0913 <2220158@ecc.ac.jp>
2024-11-20 17:05:00 +0900

    ステージを元に戻しました

--- Source/Scene/SceneGame.cpp
+++ Source/Scene/SceneGame.cpp
@@ -74,7 +74,7 @@ void SceneGame::Initialize()
 			// トランスフォーム設定
 			{
 				Transform3DComponent::Transform3DParam param{};
-				param.local_scale = DirectX::XMFLOAT3(100.0f, 1.0f, 100.0f);
+				param.local_scale = DirectX::XMFLOAT3(600.0f, 1.0f, 600.0f);
 				param.local_position = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
 				auto transform = stage->AddComponent<Transform3DComponent>(param);
 			}
@@ -269,7 +269,7 @@ void SceneGame::Initialize()
 						0.0f,
 #else
 						sinf(theta) * range,
-						0.0f,
+						0.5f,
 						cosf(theta) * range ,
 #endif
 					};
