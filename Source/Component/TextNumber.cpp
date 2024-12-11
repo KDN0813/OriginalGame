e2fe91edec1a4c795a06e28942878a5773677a12
mae0913 <2220158@ecc.ac.jp>
2024-12-11 10:17:20 +0900

    TextNumber仮作成

--- Source/Component/TextNumber.cpp
+++ Source/Component/TextNumber.cpp
@@ -1,1 +1,7 @@
 #include "TextNumber.h"
+
+void TextNumber::Start()
+{
+    // フォント用スプライト読み込み
+    this->sprite = std::make_unique<Sprite>(this->param.font_name);
+}
--- Source/Component/TextNumber.h
+++ Source/Component/TextNumber.h
@@ -1,14 +1,27 @@
 #pragma once
+#include <string>
 #include "Component.h"
+#include "Sprite\Sprite.h"
 
 class TextNumber : public Component
 {
 public:
-    TextNumber() {};
+    struct TextParam
+    {
+        std::string font_name = {};
+        DirectX::XMFLOAT2 pos = { 0.0f,0.0f };
+        int value = 0;  // 表示する値
+        float font_size = 0.0f; // １文字あたりのサイズ
+        float angle = 0.0f;
+        DirectX::XMFLOAT4 color = { 1.0f,1.0f ,1.0f ,1.0f };
+        Sprite::CENTER_TYPE center_type = Sprite::CENTER_TYPE::CENTER;
+    };
+public:
+    TextNumber(TextParam param) : param(param), default_param(param) {};
     ~TextNumber() {};
 
     // 開始関数
-    void Start()  override {};
+    void Start()  override;
     // 終了関数
     void End()  override {};
     // リスタート処理
@@ -21,6 +34,19 @@ public:
 
     // 優先度
     const COMPONENT_PRIORITY GetPriority()const noexcept  override { return COMPONENT_PRIORITY::DEFAULT; };
+
+    // 取得・設定関数
+    int GetDrawValue()const { return this->param.value; }
+    void SetDrawValue(const int v) { this->param.value = v; }
+
+private:
+    std::unique_ptr<Sprite> sprite;
+    TextParam param;
+    TextParam default_param;
+
+    DirectX::XMFLOAT2 display_size = { 1.0f,1.0f };
+    DirectX::XMFLOAT2 clip_pos = { 0.0f,0.0f };
+    DirectX::XMFLOAT2 clip_size = { 1.0f,1.0f };
 
 #ifdef _DEBUG
 public:
