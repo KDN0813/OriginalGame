41336cf5ca3947650bcbbf269c91379125449342
mae0913 <2220158@ecc.ac.jp>
2024-12-11 10:40:28 +0900

    SpriteComponentのStart関数の括弧閉じが消されていたので修正 TextNumberComponentのスプライトを読み込むとき際に、引数が間違っていたので修正

--- Source/Component/TextNumberComponent.cpp
+++ Source/Component/TextNumberComponent.cpp
@@ -3,5 +3,5 @@
 void TextNumberComponent::Start()
 {
     // フォント用スプライト読み込み
-    this->sprite = std::make_unique<Sprite>(this->param.font_name);
+    this->sprite = std::make_unique<Sprite>(this->param.font_name.c_str());
 }
