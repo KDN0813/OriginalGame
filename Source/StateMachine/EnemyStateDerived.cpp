e4d57be26f44e31e5be7e699e67df29e9a16d305
mae0913 <2220158@ecc.ac.jp>
2025-01-15 16:09:01 +0900

    EnemyStateDerivedに必要なステート側のみ作成

--- Source/StateMachine/EnemyStateDerived.cpp
+++ Source/StateMachine/EnemyStateDerived.cpp
@@ -1,15 +1,65 @@
 #include "EnemyStateDerived.h"
 
-const MyHash EnemydleState::STATE_NAME = MyHash("EnemydleState");
-EnemydleState::EnemydleState()
+const MyHash EnemyIdleState::STATE_NAME = MyHash("EnemydleState");
+EnemyIdleState::EnemyIdleState()
     :State(STATE_NAME)
 {
+    this->change_wandering_state.change_state_name = EnemyWanderingState::STATE_NAME;
+    this->change_chase_state.change_state_name = EnemyChaseState::STATE_NAME;
+    this->change_attack_state.change_state_name = EnemyAttackState::STATE_NAME;
+    this->change_damage_state.change_state_name = EnemyDamageState::STATE_NAME;
+    this->change_deth_state.change_state_name = EnemyDethState::STATE_NAME;
 }
 
-void EnemydleState::Start()
+void EnemyIdleState::Start()
 {
 }
 
-void EnemydleState::Update(float elapsed_time)
+void EnemyIdleState::Update(float elapsed_time)
+{
+}
+
+const MyHash EnemyWanderingState::STATE_NAME = MyHash("EnemyWanderingState");
+EnemyWanderingState::EnemyWanderingState()
+    :State(STATE_NAME)
+{
+    this->change_idle_state.change_state_name = EnemyIdleState::STATE_NAME;
+    this->change_chase_state.change_state_name = EnemyChaseState::STATE_NAME;
+    this->change_attack_state.change_state_name = EnemyAttackState::STATE_NAME;
+    this->change_damage_state.change_state_name = EnemyDamageState::STATE_NAME;
+    this->change_deth_state.change_state_name = EnemyDethState::STATE_NAME;
+}
+
+const MyHash EnemyChaseState::STATE_NAME = MyHash("EnemyChaseState");
+EnemyChaseState::EnemyChaseState()
+    :State(STATE_NAME)
+{
+    this->change_idle_state.change_state_name = EnemyIdleState::STATE_NAME;
+}
+
+const MyHash EnemyAttackState::STATE_NAME = MyHash("EnemyAttackState");
+EnemyAttackState::EnemyAttackState()
+    :State(STATE_NAME)
+{
+    this->change_idle_state.change_state_name = EnemyIdleState::STATE_NAME;
+}
+
+const MyHash EnemyDamageState::STATE_NAME = MyHash("EnemyDamageState");
+EnemyDamageState::EnemyDamageState()
+    :State(STATE_NAME)
+{
+    this->change_idle_state.change_state_name = EnemyIdleState::STATE_NAME;
+}
+
+const MyHash EnemyDethState::STATE_NAME = MyHash("EnemyDethState");
+EnemyDethState::EnemyDethState()
+    :State(STATE_NAME)
+{
+    this->change_deth_idle_state.change_state_name = EnemyDethIdleState::STATE_NAME;
+}
+
+const MyHash EnemyDethIdleState::STATE_NAME = MyHash("EnemyDethIdleState");
+EnemyDethIdleState::EnemyDethIdleState()
+    :State(STATE_NAME)
 {
 }
--- Source/StateMachine/EnemyStateDerived.h
+++ Source/StateMachine/EnemyStateDerived.h
@@ -5,14 +5,15 @@ class ModelAnimationControlComponent;
 class MovementComponent;
 class CharacterComponent;
 
-class EnemydleState : public State
+// 待機ステート
+class EnemyIdleState : public State
 {
 public:
 	static const MyHash STATE_NAME;
 public:
 	// コンストラクタ
-	EnemydleState();
-	~EnemydleState() {}
+	EnemyIdleState();
+	~EnemyIdleState() {}
 	// ステートに入った時のメソッド
 	void Start() override;
 	// ステートで実行するメソッド
@@ -20,12 +21,132 @@ public:
 	// ステートから出ていくときのメソッド
 	void End() override {};
 private:
-	State::ChangeState change_move_state;
+	State::ChangeState change_wandering_state;
+	State::ChangeState change_chase_state;
 	State::ChangeState change_attack_state;
 	State::ChangeState change_damage_state;
-	State::ChangeState change_dead_state;
+	State::ChangeState change_deth_state;
 private:
 	std::weak_ptr<ModelAnimationControlComponent> animation_Wprt;
 	std::weak_ptr<MovementComponent> movement_Wpt;
 	std::weak_ptr<CharacterComponent> character_Wptr;
+};
+
+// 徘徊ステート
+class EnemyWanderingState : public State
+{
+public:
+	static const MyHash STATE_NAME;
+public:
+	// コンストラクタ
+	EnemyWanderingState();
+	~EnemyWanderingState() {}
+	// ステートに入った時のメソッド
+	void Start() override {};
+	// ステートで実行するメソッド
+	void Update(float elapsed_time) override {};
+	// ステートから出ていくときのメソッド
+	void End() override {};
+private:
+	State::ChangeState change_idle_state;
+	State::ChangeState change_chase_state;
+	State::ChangeState change_attack_state;
+	State::ChangeState change_damage_state;
+	State::ChangeState change_deth_state;
+};
+
+// 接近ステート
+class EnemyChaseState : public State
+{
+public:
+	static const MyHash STATE_NAME;
+public:
+	// コンストラクタ
+	EnemyChaseState();
+	~EnemyChaseState() {}
+	// ステートに入った時のメソッド
+	void Start() override {};
+	// ステートで実行するメソッド
+	void Update(float elapsed_time) override {};
+	// ステートから出ていくときのメソッド
+	void End() override {};
+private:
+	State::ChangeState change_idle_state;
+	State::ChangeState change_chase_state;
+	State::ChangeState change_attack_state;
+};
+
+// 攻撃ステート
+class EnemyAttackState : public State
+{
+public:
+	static const MyHash STATE_NAME;
+public:
+	// コンストラクタ
+	EnemyAttackState();
+	~EnemyAttackState() {}
+	// ステートに入った時のメソッド
+	void Start() override {};
+	// ステートで実行するメソッド
+	void Update(float elapsed_time) override {};
+	// ステートから出ていくときのメソッド
+	void End() override {};
+private:
+	State::ChangeState change_idle_state;
+};
+
+// ダメージステート
+class EnemyDamageState : public State
+{
+public:
+	static const MyHash STATE_NAME;
+public:
+	// コンストラクタ
+	EnemyDamageState();
+	~EnemyDamageState() {}
+	// ステートに入った時のメソッド
+	void Start() override {};
+	// ステートで実行するメソッド
+	void Update(float elapsed_time) override {};
+	// ステートから出ていくときのメソッド
+	void End() override {};
+private:
+	State::ChangeState change_idle_state;
+};
+
+// 死亡ステート
+class EnemyDethState : public State
+{
+public:
+	static const MyHash STATE_NAME;
+public:
+	// コンストラクタ
+	EnemyDethState();
+	~EnemyDethState() {}
+	// ステートに入った時のメソッド
+	void Start() override {};
+	// ステートで実行するメソッド
+	void Update(float elapsed_time) override {};
+	// ステートから出ていくときのメソッド
+	void End() override {};
+private:
+	State::ChangeState change_deth_idle_state;
+};
+
+// 死亡待機ステート
+class EnemyDethIdleState : public State
+{
+public:
+	static const MyHash STATE_NAME;
+public:
+	// コンストラクタ
+	EnemyDethIdleState();
+	~EnemyDethIdleState() {}
+	// ステートに入った時のメソッド
+	void Start() override {};
+	// ステートで実行するメソッド
+	void Update(float elapsed_time) override {};
+	// ステートから出ていくときのメソッド
+	void End() override {};
+private:
 };
