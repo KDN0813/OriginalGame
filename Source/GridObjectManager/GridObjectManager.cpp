df8b344bb0de4b3d53ffe3138035e4697f346b4e
mae0913 <2220158@ecc.ac.jp>
2025-01-21 16:37:12 +0900

    GridObjectManagerにステージの幅など変数を追加

--- Source/GridObjectManager/GridObjectManager.cpp
+++ Source/GridObjectManager/GridObjectManager.cpp
@@ -8,6 +8,29 @@
 #endif // _DEBUG
 
 
+GridObjectManager::GridObjectManager()
+:Singleton(this) 
+{
+    // ステージのサイズ設定(ステージは正方形
+    this->STAGE_RAGE = EnemyComponent::DEFAULT_TERRITORY_RENGR;
+    
+    // 1つのエリアのサイズ
+    this->AREA_RAGE;
+
+    // 一列に何個のエリアがあるか
+    this->CELL_MAX = static_cast<int>(this->STAGE_RAGE / this->AREA_RAGE);
+
+    // エリア毎に格納するデータのコンテナのサイズ確保
+    this->grid_data_pool.resize(this->CELL_MAX * this->CELL_MAX);
+
+    const float HALF_STAGE_RAGE = this->STAGE_RAGE * 0.5f;
+        
+    // 左上の位置
+    this->min_erea_pos = { HALF_STAGE_RAGE ,0.0f,HALF_STAGE_RAGE };
+    // 右下の位置
+    this->max_erea_pos = { -HALF_STAGE_RAGE ,0.0f,-HALF_STAGE_RAGE };
+}
+
 #ifdef _DEBUG
 
 void GridObjectManager::DrawDebugGUI()
@@ -25,12 +48,12 @@ void GridObjectManager::DrawDebugPrimitive()
     const auto& debug_primitive_render = debug_manager->GetDebugPrimitiveRenderer();
     if (!debug_primitive_render) return;
 
-    for (float x = EnemyComponent::DEFAULT_TERRITORY_RENGR; x > -EnemyComponent::DEFAULT_TERRITORY_RENGR; x -= AREA_RAGE)
+    for (int i = 0; i < this->CELL_MAX * this->CELL_MAX; ++i)
     {
-        for (float z = EnemyComponent::DEFAULT_TERRITORY_RENGR; z > -EnemyComponent::DEFAULT_TERRITORY_RENGR; z -= AREA_RAGE)
-        {
-            debug_primitive_render->DrawBox({ x,-2.0f,z }, {}, { AREA_RAGE ,AREA_RAGE ,AREA_RAGE }, { 1.0f,1.0f, 1.0f, 1.0f });
-        }
+        float x = static_cast<float>(i % this->CELL_MAX);
+        float z = static_cast<float>(i / this->CELL_MAX);
+        DirectX::XMFLOAT3 pos = { this->max_erea_pos.x + x * this->AREA_RAGE,0.0f,this->max_erea_pos.z + z * this->AREA_RAGE };
+        debug_primitive_render->DrawBox(pos, {}, { this->AREA_RAGE ,this->AREA_RAGE ,this->AREA_RAGE }, { 1.0f,1.0f, 1.0f, 1.0f });
     }
 }
 
--- Source/GridObjectManager/GridObjectManager.h
+++ Source/GridObjectManager/GridObjectManager.h
@@ -1,16 +1,32 @@
 #pragma once
 
 #include "System\ClassBase\Singleton.h"
+#include <vector>
+#include <memory>
+#include <DirectXMath.h>
+
+class Object;
 
 class GridObjectManager : public Singleton<GridObjectManager>
 {
 public:
-    static constexpr float AREA_RAGE = 5.0f;    // エリアサイズ
+    // エリア毎に格納するデータ
+    struct GridData
+    {
+        std::weak_ptr<Object> object;   // 現在このエリアに入っているオブジェクト
+    };
 public:
-    GridObjectManager() :Singleton(this) {}
+    static constexpr float AREA_RAGE = 5.0f;    // エリアサイズ
+    float STAGE_RAGE{};  // ステージのサイズ
+    int CELL_MAX{};
+public:
+    GridObjectManager();
     ~GridObjectManager() {};
 
 private:
+    DirectX::XMFLOAT3 min_erea_pos;   // 全体の左上の座標
+    DirectX::XMFLOAT3 max_erea_pos;   // 全体の右下の座標
+    std::vector<GridData> grid_data_pool;
 
 #ifdef _DEBUG
 public:
