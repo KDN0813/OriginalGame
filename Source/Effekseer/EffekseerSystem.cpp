e1aad4b5923e1e76f55b3871b25504fa79231c3f
mae0913 <2220158@ecc.ac.jp>
2025-04-08 15:45:03 +0900

    EffekseerSystemにエフェクト再生関数等を実装

--- Source/Effekseer/EffekseerSystem.cpp
+++ Source/Effekseer/EffekseerSystem.cpp
@@ -56,3 +56,62 @@ void EffekseerSystem::Render()
 
     this->effekseer_render->EndRendering();
 }
+
+Effekseer::EffectRef EffekseerSystem::EffetResourceManager::LoadEffectResource(const char* filename)
+{
+    // エフェクト検索
+    EffectMap::iterator it = this->effects.find(filename);
+    // 見つかった場合
+    if (it != this->effects.end())
+    {
+        return this->effects[filename];
+    }
+
+    // エフェクト作成
+    char16_t utf16Filename[256];
+    Effekseer::ConvertUtf8ToUtf16(utf16Filename, 256, filename);
+
+    Effekseer::ManagerRef effekseer_manager = EffekseerSystem::GetInstance()->GetEffekseerManager();
+
+    // マップに登録
+    this->effects[filename] = Effekseer::Effect::Create(effekseer_manager, (EFK_CHAR*)utf16Filename);
+    return this->effects[filename];
+}
+
+void EffekseerSystem::EffectPlay(const char* filename, const DirectX::XMFLOAT3& position, float scale)
+{
+    // エフェクトリソース取得
+    Effekseer::EffectRef effekseer_effect = this->resource_manager.LoadEffectResource(filename);
+
+    // エフェクト再生
+    Effekseer::Handle handle = this->effekseer_manager->Play(effekseer_effect, position.x, position.y, position.z);
+
+    // スケール設定
+    this->effekseer_manager->SetScale(handle, scale, scale, scale);
+}
+
+void EffekseerSystem::EffectAllStop()
+{
+    this->effekseer_manager->StopAllEffects();
+}
+
+// 停止
+void EffekseerSystem::EffectStop(Effekseer::Handle handle)
+{
+    // エフェクト停止
+    this->effekseer_manager->StopEffect(handle);
+}
+
+// 座標設定
+void EffekseerSystem::EffectSetPosition(Effekseer::Handle handle, const DirectX::XMFLOAT3& position)
+{
+    // 座標設定
+    this->effekseer_manager->SetLocation(handle, position.x, position.y, position.z);
+}
+
+// スケール変更
+void EffekseerSystem::EffectSetScale(Effekseer::Handle handle, const DirectX::XMFLOAT3& scale)
+{
+    // スケール設定
+    this->effekseer_manager->SetScale(handle, scale.x, scale.y, scale.z);
+}
--- Source/Effekseer/EffekseerSystem.h
+++ Source/Effekseer/EffekseerSystem.h
@@ -9,20 +9,42 @@
 class EffekseerSystem : public Singleton<EffekseerSystem>
 {
 public:
+    class EffetResourceManager
+    {
+    public:
+        EffetResourceManager() {}
+        ~EffetResourceManager() {}
+
+        // モデルリソース読み込み
+        Effekseer::EffectRef LoadEffectResource( const char* filename);
+
+    private:
+        using EffectMap = std::map<std::string, Effekseer::EffectRef>;
+
+        EffectMap effects;
+    };
+
+public:
     EffekseerSystem() :Singleton(this) {}
     ~EffekseerSystem(){}
 
     void Initialize();
-
     void Finalize();
-
     void Update(float elapsed_time);
-
     void Render();
 
+    EffetResourceManager& GetResourceManager() { return this->resource_manager; }
     Effekseer::ManagerRef GetEffekseerManager() { return effekseer_manager; }
 
+    // エフェクト再生
+    void EffectPlay(const char* filename, const DirectX::XMFLOAT3& position, float scale = 1.0f);
+    void EffectAllStop();
+    void EffectStop(Effekseer::Handle handle);
+    void EffectSetPosition(Effekseer::Handle handle, const DirectX::XMFLOAT3& position);
+    void EffectSetScale(Effekseer::Handle, const DirectX::XMFLOAT3& scale);
 private:
     Effekseer::ManagerRef           effekseer_manager;
     EffekseerRenderer::RendererRef  effekseer_render;
+
+    EffetResourceManager            resource_manager;
 };
