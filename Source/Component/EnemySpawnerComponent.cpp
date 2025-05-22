c28333702ae857d6529f2ed396b0b0e2a5ffaabe
mae0913 <2220158@ecc.ac.jp>
2025-05-22 16:14:55 +0900

    EnemySpawnerComponentを作成

---
+++ Source/Component/EnemySpawnerComponent.cpp
@@ -0,0 +1,78 @@
+#include "EnemySpawnerComponent.h"
+
+#include "Object\Object.h"
+#include "Object\GameObjectRegistry.h"
+
+#include "System\MyMath\MyMathf.h"
+#include "Object\Object.h"
+#include "Object\Constant\EnemyConstant.h"
+
+#include "Component\TransformComponent.h"
+
+EnemySpawnerComponent::EnemySpawnerComponent(const std::shared_ptr<ObjectManager>& manager)
+    :object_manager_Wptr(manager)
+    , create_cool_time_max(3.0f)
+    , create_cool_time_min(5.0f)
+    , create_cool_timer()
+    , is_create_enemy
+    (
+#ifdef _DEBUG
+        false
+#else
+        true
+#endif // _DEBUG
+    )
+{
+}
+
+void EnemySpawnerComponent::Update(float elapsed_time)
+{
+    if (!this->is_create_enemy) return;
+
+    // 敵の生成
+    this->create_cool_timer -= elapsed_time;
+    if (this->create_cool_timer <= 0.0f)
+    {
+        AddCreateEnemy(this->object_manager_Wptr.lock());
+
+        const float COOL_TIME = MyMath::RandomRange(this->create_cool_time_min, this->create_cool_time_max);
+        this->create_cool_timer = COOL_TIME;
+    }
+}
+
+void EnemySpawnerComponent::AddCreateEnemy(const std::shared_ptr<ObjectManager>& manager)
+{
+    if (manager == nullptr) return;
+
+    GameObjectRegistry::Instance object_registry = GameObjectRegistry::GetInstance();
+
+    const auto& player = object_registry->GetPlayer();
+    if (!player) return;
+    const auto& player_transform = player->GetComponent<Transform3DComponent>(this->player_transform_Wptr);
+    if (!player_transform) return;
+    const DirectX::XMFLOAT3 PLAYER_POS = player_transform->GetWorldPosition();
+
+    const std::vector<std::weak_ptr<Object>>& enemy_Wptr_pool = object_registry->GetEnemyWptPool();
+
+    const size_t NOW_ENEMY_COUNT = enemy_Wptr_pool.size();  // 現在の敵の数
+    const size_t ENEMY_MAX = EnemyConstant::ENEMY_MAX;      // 敵の最大値
+#ifdef _DEBUG
+    const size_t CREATE_ENEMY_MAX = MyMath::RandomRange(10, 20);	// 1度に生成できるエネミーの最大数
+#else
+    const size_t CREATE_ENEMY_MAX = MyMath::RandomRange(80, 150);	// 1度に生成できるエネミーの最大数
+#endif // _DEBUG
+
+    for (int i = 0; i < ENEMY_MAX - NOW_ENEMY_COUNT; ++i)
+    {
+        if (CREATE_ENEMY_MAX <= i)break;
+
+        const float player_area_rage = 20.0f;
+        const DirectX::XMFLOAT3 spawn_point = MyMath::GetNonOverlappingPointInRing(PLAYER_POS, player_area_rage, EnemyConstant::NEAR_PLAYER_TERRITORY_RENGR);
+
+        // 敵の生成
+        const auto& enemy = EnemyConstant::CreateEnemy(spawn_point, manager->Create());
+        // 敵をGameObjectRegistryに設定
+        object_registry->SetEnemy(enemy);
+    }
+
+}
---
+++ Source/Component/EnemySpawnerComponent.h
@@ -0,0 +1,62 @@
+#pragma once
+#include "Component.h"
+#include <vector>
+
+class Object;
+class ObjectManager;
+class Transform3DComponent;
+
+// エネミーの生成を行うコンポーネント
+class EnemySpawnerComponent : public Component
+{
+public:
+    EnemySpawnerComponent(const std::shared_ptr<ObjectManager>& manager);
+    ~EnemySpawnerComponent() {};
+
+    // 開始関数
+    void Start()  override {};
+    // 終了関数
+    void End()  override {};
+    // リスタート処理
+    void ReStart() override {};      // パラメータの初期化
+    // 更新関数
+    void Update(float elapsed_time) override;
+
+    // 名前取得
+    const char* GetName()const  override { return "EnemySpawner"; };
+    // 優先度
+    const PRIORITY GetPriority()const noexcept  override { return PRIORITY::DEFAULT; };
+
+    void AddCreateEnemy(const std::shared_ptr<ObjectManager>& manager);
+private:
+    std::weak_ptr<ObjectManager> object_manager_Wptr;
+    float create_cool_time_max;
+    float create_cool_time_min;
+    float create_cool_timer;    // 生成のクールタイム
+    bool is_create_enemy;
+
+private:
+    std::weak_ptr<Object> player_Wptr;
+    std::weak_ptr<Transform3DComponent> player_transform_Wptr;
+    std::weak_ptr<Object> stage_floor_Wptr;
+    std::weak_ptr<Object> stage_wall_Wptr;
+    std::vector<std::weak_ptr<Object>> enemy_Wptr_pool;
+
+#ifdef _DEBUG
+public:
+    /**
+     * デバックの情報を2D画面に出力する関数
+     */
+    void DrawDebugGUI()  override {};
+    /**
+     * デバックの情報を3D画面上に出力する関数
+     */
+    void DrawDebugPrimitive()  override {};
+    /**
+     * デバッグプリミティブ表示用ImGui
+     */
+    void DrawDebugPrimitiveGUI()  override {};
+    bool IsDebugPrimitive() override { return false; }   // DebugPrimitiveが存在するか
+#endif // DEBUG
+};
+
